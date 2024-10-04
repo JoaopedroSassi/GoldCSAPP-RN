e5040937a4b000eacdb1e4cc36cbdde4f3d6b367
JoaopedroSassi <joaopedro10813@gmail.com>
2024-10-04 01:11:29 -0300

    Renomeando tela para ConfirmationStock

--- app/confirmation-stock.tsx
+++ app/confirmation-stock.tsx
@@ -3,7 +3,7 @@ import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 import BottomNav from "./components/bottomNav";
 import { useNavigation } from "@react-navigation/native";
 
-const BeforeAddStock: React.FC = () => {
+const ConfirmationStock: React.FC = () => {
   const navigation = useNavigation();
   const navigateToHome = () => {
     // Lógica de navegação para a Home
@@ -112,4 +112,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default BeforeAddStock;
+export default ConfirmationStock;
