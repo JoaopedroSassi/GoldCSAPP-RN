0d366d958ee0cd1a144ce20ee58848ec18b392ac
JoaopedroSassi <joaopedro10813@gmail.com>
2024-10-04 01:07:54 -0300

    Add texto barra de navegação

--- app/components/bottomNav.tsx
+++ app/components/bottomNav.tsx
@@ -1,28 +1,37 @@
-import React from 'react';
-import { View, TouchableOpacity, StyleSheet } from 'react-native';
-import Icon from 'react-native-vector-icons/MaterialIcons';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import React from "react";
+import { View, TouchableOpacity, StyleSheet, Text } from "react-native";
+import Icon from "react-native-vector-icons/MaterialIcons";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
 interface BottomNavProps {
   navigateToHome: () => void;
   navigateToPedidos: () => void;
 }
 
-const BottomNav: React.FC<BottomNavProps> = ({ navigateToHome, navigateToPedidos }) => {
+const BottomNav: React.FC<BottomNavProps> = ({
+  navigateToHome,
+  navigateToPedidos,
+}) => {
   return (
     <View style={styles.container}>
       <TouchableOpacity onPress={navigateToHome} style={styles.button}>
         <Icon name="home" size={30} color="#FFFFFF" />
+        <Text style={styles.navText}>Início</Text>
       </TouchableOpacity>
 
       <TouchableOpacity onPress={navigateToPedidos} style={styles.boxButton}>
         <View style={styles.circle}>
-          <MaterialCommunityIcons name="cube-outline" size={30} color="#1A1ABB" />
+          <MaterialCommunityIcons
+            name="cube-outline"
+            size={30}
+            color="#1A1ABB"
+          />
         </View>
       </TouchableOpacity>
 
       <TouchableOpacity onPress={navigateToPedidos} style={styles.button}>
         <Icon name="list-alt" size={30} color="#FFFFFF" />
+        <Text style={styles.navText}>Pedidos</Text>
       </TouchableOpacity>
     </View>
   );
@@ -30,26 +39,32 @@ const BottomNav: React.FC<BottomNavProps> = ({ navigateToHome, navigateToPedidos
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    backgroundColor: '#1A1ABB',
+    flexDirection: "row",
+    justifyContent: "space-around",
+    backgroundColor: "#1A1ABB",
     paddingVertical: 10,
   },
   button: {
-    alignItems: 'center',
+    alignItems: "center",
     padding: 10,
+    marginTop: 10,
   },
   boxButton: {
-    alignItems: 'center',
+    alignItems: "center",
     padding: 10,
   },
   circle: {
     width: 60,
     height: 60,
-    backgroundColor: '#FFD700', // Círculo amarelo
+    backgroundColor: "#FFD700",
     borderRadius: 30,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  navText: {
+    color: "#FFFFFF",
+    fontSize: 12,
+    marginTop: 4,
   },
 });
 
