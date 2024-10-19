62df7db7060bc488fb7e86ef1456a975ad1c03e6
JoaopedroSassi <joaopedro10813@gmail.com>
2024-10-19 16:29:21 -0300

    Ajustes CSS addStock

--- app/addStock.tsx
+++ app/addStock.tsx
@@ -1,106 +1,120 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
-    View,
-    Text,
-    TextInput,
-    TouchableOpacity,
-    StyleSheet,
-} from 'react-native';
-import BottomNav from './components/bottomNav';
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
+import BottomNav from "./components/bottomNav";
+import Icon from "react-native-vector-icons/MaterialIcons";
+import { router } from "expo-router";
 
 const AddStock: React.FC = () => {
-    const [product, setProduct] = useState('');
-    const [quantity, setQuantity] = useState('');
+  const [product, setProduct] = useState("");
+  const [quantity, setQuantity] = useState("");
 
-    const handleContinue = () => {
-        // Lógica de adição ao estoque
-        console.log('Produto:', product, 'Quantidade:', quantity);
-    };
+  const handleNavigation = () => {
+    router.push("/home");
+  };
 
-    return (
-        <View style={styles.container}>
-            <View style={styles.header}>
-                <Text style={styles.headerTitle}>Estoque</Text>
-            </View>
+  const handleContinue = () => {
+    console.log("Produto:", product, "Quantidade:", quantity);
+  };
 
-            {/* Formulário */}
-            <View style={styles.form}>
-                <TextInput
-                    style={styles.input}
-                    placeholder="Produto"
-                    value={product}
-                    onChangeText={setProduct}
-                    placeholderTextColor="#6A6A6A"
-                />
-                <TextInput
-                    style={styles.input}
-                    placeholder="Quantidade"
-                    value={quantity}
-                    onChangeText={setQuantity}
-                    placeholderTextColor="#6A6A6A"
-                />
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Icon
+          name="arrow-back"
+          size={30}
+          color="#000"
+          onPress={handleNavigation}
+        />
+        <Text style={styles.headerText}>Estoque</Text>
+      </View>
 
-                <TouchableOpacity style={styles.button} onPress={handleContinue}>
-                    <Text style={styles.buttonText}>CONTINUAR</Text>
-                </TouchableOpacity>
-            </View>
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Produto"
+          value={product}
+          onChangeText={setProduct}
+          placeholderTextColor="#6A6A6A"
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Quantidade"
+          value={quantity}
+          onChangeText={setQuantity}
+          placeholderTextColor="#6A6A6A"
+        />
 
-            <BottomNav />
-        </View>
-    );
+        <TouchableOpacity style={styles.button} onPress={handleContinue}>
+          <Text style={styles.buttonText}>CONTINUAR</Text>
+        </TouchableOpacity>
+      </View>
+
+      <BottomNav />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#FFFFFF',
-      justifyContent: 'space-between',
+  container: {
+    flex: 1,
+    backgroundColor: "#FFFFFF",
+    justifyContent: "space-between",
+  },
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 16,
+    shadowColor: "#000000",
+    shadowOffset: {
+      width: 0,
+      height: 3,
     },
-    header: {
-      flexDirection: 'row',
-      alignItems: 'center',
-      backgroundColor: '#FFFFFF',
-      paddingVertical: 15,
-      paddingHorizontal: 10,
-      borderBottomWidth: 1,
-      borderBottomColor: '#E0E0E0',
-    },
-    backButton: {
-      marginRight: 10,
-    },
-    backButtonText: {
-      fontSize: 24,
-      color: '#000',
-    },
-    headerTitle: {
-      fontSize: 20,
-      fontWeight: 'bold',
-      color: '#000',
-    },
-    form: {
-      padding: 20,
-      marginTop: 20,
-    },
-    input: {
-      borderWidth: 1,
-      borderColor: '#6A6A6A',
-      borderRadius: 8,
-      marginBottom: 20,
-      padding: 10,
-      fontSize: 16,
-      color: '#000',
-    },
-    button: {
-      backgroundColor: '#FFC107',
-      paddingVertical: 15,
-      borderRadius: 8,
-      alignItems: 'center',
-    },
-    buttonText: {
-      color: '#000',
-      fontSize: 18,
-      fontWeight: 'bold',
-    },
-  });
+    shadowOpacity: 0.3,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  headerText: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginLeft: 16,
+  },
+  backButton: {
+    marginRight: 10,
+  },
+  backButtonText: {
+    fontSize: 24,
+    color: "#000",
+  },
+  form: {
+    padding: 20,
+    marginTop: 20,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#6A6A6A",
+    borderRadius: 8,
+    marginBottom: 20,
+    padding: 10,
+    fontSize: 16,
+    color: "#000",
+  },
+  button: {
+    backgroundColor: "#FFC107",
+    paddingVertical: 15,
+    borderRadius: 8,
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "#000",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+});
 
 export default AddStock;
